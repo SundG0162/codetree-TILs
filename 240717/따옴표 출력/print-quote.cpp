9867ffe13b7deada3cdd079eedf315e13678125b
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-17 12:08:13 +0900

    Title: 따옴표 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240717/따옴표 출력/print-quote.cpp
+++ 240717/따옴표 출력/print-quote.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "He says \"It's a really simple sentence\"."
-    // 여기에 코드를 작성해주세요.
-    return 0;
+    cout << "He says \"It's a really simple sentence\".";
+        // 여기에 코드를 작성해주세요.
+        return 0;
 }
