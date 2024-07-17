3b4b5a080370485b0127409027767ee12dcf397b
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-17 12:40:00 +0900

    Title: 실수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a,b;
+    cin >> a >> b;
+    cout.precision(2);
+    cout << fixed;
+    cout << a +b;
+    return 0;
+}
