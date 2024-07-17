b9cfef52fc32ef23f7f4f08644d4aae611e06a91
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-17 12:40:47 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+    float a,b,c;
+    cin >> a >> b >> c;
+    cout.precision(3);
+    cout << fixed;
+    cout << a << endl << b << endl << c << endl;
+    return 0;
+}
