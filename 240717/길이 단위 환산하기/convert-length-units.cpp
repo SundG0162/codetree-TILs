a150600d8a2baee08acd0c9197c04a077ca73b8b
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-17 12:35:03 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a = 0;
+    cin >> a;
+    cout.precision(1);
+    cout << fixed;
+    cout << a * 30.48f;
+    return 0;
+}
