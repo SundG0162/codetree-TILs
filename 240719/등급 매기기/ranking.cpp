2404ecfc8061aa36810eb0ba79739779449c51e5
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-19 23:54:44 +0900

    Title: 등급 매기기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240719/등급 매기기/ranking.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    n >= 90 ? cout << "A" : n >= 80 ? cout << "B" : n <= 70 ? cout << "C" : n <= 60 ? cout << "D" : cout << "F";
+    return 0;
+}
