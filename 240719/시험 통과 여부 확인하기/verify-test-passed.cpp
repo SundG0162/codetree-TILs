2cd8c41a7984d89aeefa66e419ca6be6f853f1f5
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-19 23:42:04 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/시험 통과 여부 확인하기/verify-test-passed.cpp
+++ 240719/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    n >= 80 ? cout << "pass" : cout << 80 - n << " more scores";
+    n >= 80 ? cout << "pass" : cout << 80 - n << " more score";
     return 0;
 }
