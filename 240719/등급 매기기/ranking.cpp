e2966caa6d14f91b9026ba7e131b7fe34cbfa48c
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-19 23:55:34 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/등급 매기기/ranking.cpp
+++ 240719/등급 매기기/ranking.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    n >= 90 ? cout << "A" : n >= 80 ? cout << "B" : n >= 70 ? cout << "C" : n <= 60 ? cout << "D" : cout << "F";
+    n >= 90 ? cout << "A" : n >= 80 ? cout << "B" : n >= 70 ? cout << "C" : n >= 60 ? cout << "D" : cout << "F";
     return 0;
 }
