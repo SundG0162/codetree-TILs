3641b8c00b9115fa0de443fbf4949c2f904a8395
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-19 23:51:55 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/살 수 있는 물건/things-able-to-buy.cpp
+++ 240719/살 수 있는 물건/things-able-to-buy.cpp
@@ -5,6 +5,6 @@ int main() {
     int n;
     cin >> n;
 
-    n >= 3000 ? cout << "book" : n >= 1000 ? cout << "1000" : cout << "no";
+    n >= 3000 ? cout << "book" : n >= 1000 ? cout << "mask" : cout << "no";
     return 0;
 }
