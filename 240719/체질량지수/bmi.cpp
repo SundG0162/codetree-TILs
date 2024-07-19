0b23f2103e8e2d55fe67c5fb0eeffe8df906787e
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-19 23:38:33 +0900

    Title: 체질량지수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240719/체질량지수/bmi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k,w;
+    cin >> k >> w;
+    int b = (10000 * 2) / (h * h);   
+    cout << b;
+    if(b >= 25)
+    {
+        cout << endl << "Obesity";
+    }
+    return 0;
+}
