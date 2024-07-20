acde27ff229ea8f5b22dc71edd44368abf3998cd
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-20 21:19:31 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/장학금/scholarship.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    int money = 0;
+
+    money = (b >= 95 ? 100000 : b >= 90 ? 50000 : 0);
+    if(a <= 90)
+        money = 0;
+
+        cout << money;
+    return 0;
+}
