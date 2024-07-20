bd7a59dce33ced445ab1382a1ac004f085a78ee1
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-20 21:13:00 +0900

    Title: 범위 밖의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240720/범위 밖의 수/number-out-of-range.cpp
+++ 240720/범위 밖의 수/number-out-of-range.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     int a;
     cin >> a;
-    a > 10 || a > 20 ? cout << "yes" : cout << "no";
+    a < 10 || a > 20 ? cout << "yes" : cout << "no";
     return 0;
 }
