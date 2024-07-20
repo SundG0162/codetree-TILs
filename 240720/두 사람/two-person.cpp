5e9dae73af9dc09628d55cc270caf46448f70972
SundG0162 <janghyunwoo0162@gmail.com>
2024-07-20 21:24:24 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/두 사람/two-person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,c;
+    char b,d;
+    cin >> a >> b >> c >> d;
+
+    cout << ((a >= 19 && b == 'M') || (c >= 19 &&  d == 'M'));
+    return 0;
+}
